1bb4c1a788574bb19a3cffb26315274f71d1985a
Celso Wo <celso.wo@tegra.me>
2017-03-19 00:25:30 -0300

    Added template folder and a sample template

--- lazy-init.js
+++ lazy-init.js
@@ -8,17 +8,34 @@ const cwd = process.cwd();
 
 console.log("\n  lazy init");
 
-if (fs.existsSync(`cwd/.lazy`)) {
+if (fs.existsSync(`${cwd}/.lazy`)) {
   console.log(`\n  Lazy project folder found, it doesn't need to initialize a new one.`);
 } else {
   console.log(`\n  Creating lazy project folder...`);
-  fs.mkdir(`${cwd}/.lazy`);
+  fs.mkdirSync(`${cwd}/.lazy`);
 
   console.log(`  Creating settings file...`);
   var fd = fs.openSync(`${cwd}/.lazy/lazy-settings.json`, 'w');
   fs.writeSync(fd, "{");
   fs.writeSync(fd, "\n");
   fs.writeSync(fd, "}");
+  fs.closeSync(fd);
+
+  console.log("\n  Creating sample template");
+  fs.mkdirSync(`${cwd}/.lazy/templates`);
+  fs.mkdirSync(`${cwd}/.lazy/templates/sample`);
+  fd = fs.openSync(`${cwd}/.lazy/templates/sample/template-settings.json`, 'w');
+  fs.writeSync(fd, "{");
+  fs.writeSync(fd, "\n");
+  fs.writeSync(fd, "}");
+  fs.closeSync(fd);
+
+  fd = fs.openSync(`${cwd}/.lazy/templates/sample/express.js`, 'w');
+  fs.writeSync(fd, "var express = require('express');\n");
+  fs.writeSync(fd, "var app = express();\n");
+  fs.writeSync(fd, "app.get('/', function (req, res) {\n");
+  fs.writeSync(fd, "  res.send('hello world');\n");
+  fs.writeSync(fd, "});\n");
   fs.closeSync(fd);
 
   console.log(`\n  Completed!`);
